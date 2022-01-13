6e1f1ce4057c8157bdb4cb19761ce5e49ed042a5
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-01-13 17:59:54 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Home from './pages/Home'
 import Categories from './pages/Categories'
 import Basket from './pages/Basket'
 import SingleProduct from './pages/SingleProduct'
+import Electronics from './pages/categories/Electronics'
+import Jewelery from './pages/categories/Jewelery'
+import MenClothing from './pages/categories/MenClothing'
+import WomenClothing from './pages/categories/WomenClothing'
 import './styles/index.css'
 
 
@@ -16,13 +20,14 @@ function App() {
       <Header />
 
       <Routes>
+
         <Route path='/home' element={<Home />} />
-
-
         <Route path='/home/:id' element={<SingleProduct />} />
-
         <Route path='/categories' element={<Categories />} />
-
+        <Route path='/categories/:id' element={<Electronics />} />
+        <Route path='/categories/:id' element={<Jewelery />} />
+        <Route path='/categories/:id' element={<MenClothing />} />
+        <Route path='/categories/:id' element={<WomenClothing />} />
         <Route path='/basket' element={<Basket />} />
 
       </Routes>
